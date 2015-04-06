0501f181b61fa21d0f467ae2b903cb39670ec754
svanlaere <samuelvanlaere@gmail.com>
2015-04-06 15:39:11 +0200

    German i18n
    German i18n provided by Tina

---
+++ i18n/de-message.php
@@ -0,0 +1,15 @@
+<?php
+/* Security measure */
+if (!defined('IN_CMS')) {
+    exit();
+}
+
+/**
+ * Disable filters plugin - German
+ */
+
+return array(
+    'Disable filters' => '',
+	'Disables filters for specific partnames' => '',
+    'Select the part for which you want to disable the filters' => '',
+);
